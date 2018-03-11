cfbd78107eda88c847ccba4fd01999e8b3c62a2e
Sagar Ganapaneni <123saga@gmail.com>
2018-03-11 15:20:11 -0700

    getlocations with online Online flag

--- weathertrends/R/getLocations.R
+++ weathertrends/R/getLocations.R
@@ -1,11 +1,19 @@
-getAllLocations <- function(){
+getAllLocations <- function(online=FALSE){
   
-  Locations <- readRDS("Distance_data_master.rda")
+  if(online==TRUE){
+    URL <- "https://www.ncdc.noaa.gov/crn/api/v1.0/sites"
+    
+    Locations <- fromJSON(RCurl::getURL(URL))
+  } else{
+    
+    Locations <- readRDS("Distance_data_master.rda")
+  }
+    
+    Locations <- Locations%>%
+      dplyr::select(state,location)
+    
+    Locations <- unique(Locations)             
   
-  Locations <- Locations%>%
-    dplyr::select(state,location)
-  
-  Locations <- unique(Locations)             
   
   #return
   Locations
