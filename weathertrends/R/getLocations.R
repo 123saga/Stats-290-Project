4da6d6790c043a6fb2bf56a72f6c57b59e2bd113
Sagar Ganapaneni <123saga@gmail.com>
2018-03-11 16:09:23 -0700

    updated getlocations fn

--- weathertrends/R/getLocations.R
+++ weathertrends/R/getLocations.R
@@ -1,4 +1,6 @@
-getAllLocations <- function(online=FALSE){
+getAllLocations <- function(online=FALSE,state="CA"){
+  
+  st <- state
   
   if(online==TRUE){
     URL <- "https://www.ncdc.noaa.gov/crn/api/v1.0/sites"
@@ -6,15 +8,21 @@ getAllLocations <- function(online=FALSE){
     Locations <- fromJSON(RCurl::getURL(URL))
   } else{
     
-    Locations <- readRDS("Distance_data_master.rda")
+    Locations <- readRDS("Locations.rda")
   }
     
+    if(st %in% (unique(Locations$state))){
     Locations <- Locations%>%
-      dplyr::select(state,location)
+      dplyr::select(state,location)%>%
+      filter(state==st)
     
-    Locations <- unique(Locations)             
+    Locations <- unique(Locations)
+    
+    #return
+    Locations
+    }else{
+      print("Invalid State code, Please enter a valid state code")
+    }
   
-  
-  #return
-  Locations
+
 }
