096259235e90275416eb09464e9de42b0602f8f5
learnlifelong01 <vikram.dceian@gmail.com>
2018-03-12 11:32:13 -0700

    conditional installation

--- weathertrends/R/loading libraries.R
+++ weathertrends/R/loading libraries.R
@@ -1,3 +1,14 @@
+## Install the package if needed
+installIfNeeded <- function(packages, ...) {
+        for (package in packages) {
+                installedPackages <- installed.packages()[, 1]
+                if (! (package %in% installedPackages))
+                        suppressMessages(install.packages(package, ...))
+        }
+}
+installIfNeeded(c("RCurl","jsonlite","tidyverse","lubridate","ggplot2","ggmap","ggExtra","gridExtra","grid"))
+
+
 # loading libraries
 library(RCurl)
 library(jsonlite)
@@ -12,3 +23,4 @@ library(ggplot2)
 library(ggmap)
 library(ggExtra)
 library(gridExtra)
+library(grid)
