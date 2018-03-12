2679e58906aff6ad156963a0ca112da1528ed6aa
learnlifelong01 <vikram.dceian@gmail.com>
2018-03-11 18:54:15 -0700

    use this file to load libraries

---
+++ weathertrends/R/loading libraries.R
@@ -0,0 +1,14 @@
+# loading libraries
+library(RCurl)
+library(jsonlite)
+
+library(tidyverse)
+
+# date libs
+library(lubridate)
+
+# plot libs
+library(ggplot2)
+library(ggmap)
+library(ggExtra)
+library(gridExtra)
