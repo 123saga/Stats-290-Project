9cd675f62793eec144fc97fd826ab38f543b536b
Sagar Ganapaneni <123saga@gmail.com>
2018-03-11 19:25:47 -0700

    updated spacial plot with lables

--- weathertrends/R/plot_spatial.R
+++ weathertrends/R/plot_spatial.R
@@ -1,9 +1,15 @@
-
-
+# function to plot spacial heap map:
 
 plot_spatial <- function(online=FALSE,Date=as.Date("2017-06-01"), measure="t_max") {
   
   onl<- online
+  
+  # prepare lables for plot
+  metrics_desc_map <- readRDS("Metrics.rda")
+  metrics_desc_map <- metrics_desc_map[c("id","description")]
+  measures <- c("p_official","rh_std","solarad","t_max","t_min","t_official","windspd","ws_max")
+  metrics_desc_map <- subset(metrics_desc_map,id %in% measures)
+  metrics_desc_map$display_text <- paste0(toupper(metrics_desc_map$id)," [",metrics_desc_map$description,"]")
   
   data <-  getSpatialPlotData(online=onl,date = Date, metric = measure)
   
@@ -22,6 +28,7 @@ plot_spatial <- function(online=FALSE,Date=as.Date("2017-06-01"), measure="t_max
     scale_color_gradientn(colours =                        
                             c('#a50026','#d73027','#f46d43','#fdae61','#abd9e9','#74add1','#4575b4','#313695'))+
     theme(legend.position = 'none')
+  
   #Hawaii
   map<-get_map(location='Hawaii', zoom=8, maptype = "terrain",
                source='google',color='bw')
@@ -29,10 +36,13 @@ plot_spatial <- function(online=FALSE,Date=as.Date("2017-06-01"), measure="t_max
     scale_color_gradientn(colours =                        
                             c('#a50026','#d73027','#f46d43','#fdae61','#abd9e9','#74add1','#4575b4','#313695'))+
     theme(legend.position = 'none')
-
+  
   #plotting on map
   options(warn=-1)
-  collage <- grid.arrange(CS,AK, HI,layout_matrix = rbind(c(2,2,1,1,1,1), c(3,3,1,1,1,1)), top = paste0("Spatial plot for:",measure))
+  collage <- grid.arrange(CS,AK, HI,layout_matrix = rbind(c(2,2,1,1,1,1), c(3,3,1,1,1,1)),
+                          top = textGrob(paste0("Spatial plot:",metrics_desc_map[which(metrics_desc_map$id==measure),c("display_text")]),
+                                         gp=gpar(fontsize=20)))
+    
   options(warn=0)            
   return(collage)
   
