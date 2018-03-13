951722ad5aad4ca5702195a88f2f9b60dd3c4534
Sagar Ganapaneni <123saga@gmail.com>
2018-03-12 20:15:59 -0700

    Add files via upload

---
+++ weathertrends/getLocations.R
@@ -0,0 +1,37 @@
+getAllLocations <- function(online=TRUE,state=NA){
+  
+  st <- state
+  URL <- "https://www.ncdc.noaa.gov/crn/api/v1.0/sites"
+  Locations <- fromJSON(RCurl::getURL(URL))
+  
+  # save locations information
+  
+  if(online==TRUE){
+    URL <- "https://www.ncdc.noaa.gov/crn/api/v1.0/sites"
+    Locations <- fromJSON(RCurl::getURL(URL))
+    saveRDS(Locations,'Locations.rda')
+    
+  } else{
+    
+    Locations <- readRDS('Locations.rda')
+  }
+  
+  if(st %in% (unique(Locations$state)) | is.na(st)){
+    Locations <- Locations%>%
+      dplyr::select(state,location,id)
+    
+    if(!is.na(st)){
+      Locations <- Locations%>%
+        filter(state==st)
+    }
+    
+    Locations <- unique(Locations)
+    
+    #return
+    Locations
+  }else{
+    print("Invalid State code, Please enter a valid state code")
+  }
+  
+  
+}
