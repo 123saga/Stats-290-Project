f07f474b72f8edd77e723bac5d309af6486429a6
learnlifelong01 <vikram.dceian@gmail.com>
2018-03-12 23:46:28 -0700

    fixed overwritting mixup

--- weathertrends/R/plot_spatial.R
+++ weathertrends/R/plot_spatial.R
@@ -20,7 +20,7 @@ plot_spatial <- function(online=FALSE,Date=as.Date("2017-06-01"), measure="t_max
                                   source='google', color='bw'))
   
   CS<-ggmap(map) + geom_point(data = data, aes(x=longitude, y=latitude, color = value), alpha=0.9, size=5) + 
-    scale_color_gradientn(colours =                        
+          theme_minimal(base_size = 20) + scale_color_gradientn(colours =                        
                             c('#a50026','#d73027','#f46d43','#fdae61','#abd9e9','#74add1','#4575b4','#313695'))
   
   #Alaska
@@ -28,23 +28,22 @@ plot_spatial <- function(online=FALSE,Date=as.Date("2017-06-01"), measure="t_max
                                 source='google',color='bw'))
   
   AK <- ggmap(map) + geom_point(data = data, aes(x=longitude, y=latitude, color = value), alpha=1, size=5) + 
-    scale_color_gradientn(colours =                        
+          theme_minimal(base_size = 20) + scale_color_gradientn(colours =                        
                             c('#a50026','#d73027','#f46d43','#fdae61','#abd9e9','#74add1','#4575b4','#313695'))+
     theme(legend.position = 'none')
   #Hawaii
   suppressMessages(map<-get_map(location='Hawaii', zoom=8, maptype = "terrain",
                                 source='google',color='bw'))
   HI <- ggmap(map) + geom_point(data = data, aes(x=longitude, y=latitude, color = value), alpha=1, size=5) + 
-    scale_color_gradientn(colours =                        
+          theme_minimal(base_size = 20) + scale_color_gradientn(colours =                        
                             c('#a50026','#d73027','#f46d43','#fdae61','#abd9e9','#74add1','#4575b4','#313695'))+
     theme(legend.position = 'none')
   
   #plotting on map
   options(warn=-1)
-  collage <- grid.arrange(CS,AK, HI,layout_matrix = rbind(c(2,2,1,1,1,1), c(3,3,1,1,1,1)),
+  return(grid.arrange(CS,AK, HI,layout_matrix = rbind(c(2,2,1,1,1,1), c(3,3,1,1,1,1)),
                           top = textGrob(plot_title,
-                                         gp=gpar(fontsize=20)))
+                                         gp=gpar(fontsize=20))))
   options(warn=0)            
-  return(plot(collage))
-  
+
 }
